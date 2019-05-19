fef0b9afb95d4c465d8906fde273d01e00ff8ea3
Leonardo Rodrigues <leonardo.rxs@hotmail.com>
2019-05-19 19:56:39 -0300

    Add project files.

---
+++ src/RxS.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Modulos.Produtos.Controllers
+{
+    [Area("Produtos")]
+    [Route("Produtos")]
+    public class CadastroController : Controller
+    {
+        [Route("Lista")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Busca")]
+        public IActionResult Busca()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RxS.UI.Site/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.UI.Site
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapAreaRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id:int?}");
+                routes.MapAreaRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id:int?}");
+
+                //routes.MapRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id:int?}");
+
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id:int?}");
+            });
+        }
+    }
+}
